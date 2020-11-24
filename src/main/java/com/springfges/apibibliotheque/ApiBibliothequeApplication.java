9e48e53850b092ff4e4b5035d40611d58e5ff7d2
Pierre FONTAINE <pierrefontaine00@gmail.com>
2020-11-24 15:26:27 +0100

    :tada: Initial commit.

---
+++ src/main/java/com/springfges/apibibliotheque/ApiBibliothequeApplication.java
@@ -0,0 +1,13 @@
+package com.springfges.apibibliotheque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBibliothequeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBibliothequeApplication.class, args);
+	}
+
+}
