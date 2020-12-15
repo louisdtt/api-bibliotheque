80fa8e6109646c29426288b5657d2420a8d356dd
Pierre Fontaine <pierrefontaine00@gmail.com>
2020-12-15 15:21:15 +0100

    :sparkles: Add search endpoints for comments.

--- src/main/java/com/springfges/apibibliotheque/comment/CommentRepository.java
+++ src/main/java/com/springfges/apibibliotheque/comment/CommentRepository.java
@@ -3,6 +3,9 @@ package com.springfges.apibibliotheque.comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.Optional;
+
 @RepositoryRestResource(path="comment")
 public interface CommentRepository  extends JpaRepository<Comment, Integer>{
+    Optional<Comment> findByTitle(String title);
 }
